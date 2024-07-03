6194cb0c0ef402ee01073ee2acee91b15edcaaa4
Zahra Arres <arreszahra0@gmail.com>
2024-07-03 17:06:58 +0100

    first commit

---
+++ typereact/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Greeting from './code1';
+import Counter from './code2';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name='Zahra' />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
